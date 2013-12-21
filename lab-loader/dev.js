440a0fd4cc7c475df265bd9ac047aeadc5d4aa6c
Xingchen Hong <hello@xc-h.net>
2013-12-21 17:58:32 -0600

    added default page title

--- lab-loader/dev.js
+++ lab-loader/dev.js
@@ -34,7 +34,7 @@ var s = document.getElementsByTagName('script')[0];
 
 		// framework
 		var framework = {
-			version : '0.2.2.7',
+			version : '0.2.2.8',
 			// console
 			console: {
 				$container : $DIV()
